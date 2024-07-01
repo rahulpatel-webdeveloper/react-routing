8d82786e932d09ed97a954412eba7515e594a1eb
Rahul <rahulpat.rp@gmail.com>
2024-07-01 17:27:42 +0530

    footer changes

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
     <footer class="bg-gray-900 h-16">
-    <p class="pt-5 text-center text-white"> 2024 TechArk Solutions – All Rights Reserved</p>
+    <p class="pt-5 text-center text-white">Design and Develop by Rahul Patel & Rutvik Thakkar</p>
 </footer>
     </>
   )
